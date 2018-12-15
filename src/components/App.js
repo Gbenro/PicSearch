593453c3aa275eb0286676fdb98d3c23e6dc96af
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-14 19:23:07 -0600

    added unsplash Api

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import SearchBar from './SearchBar'
+
 class App extends Component {
   onSearchSubmit (term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID f363802431dc7a5495fdc1a5ef028884404e5ee1d32ee52e4d258cb3b2ce188d'
+      }
+    })
   }
   render () {
     return (
